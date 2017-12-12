530336d071dcb9b10f1eb9cb7f5c49ce78c4fbb4
Dean Gibson <ldeangibson2@gmail.com>
2017-12-11 16:06:26 -0800

    market shows all items not just the first 8

--- src/popmarket.ts
+++ src/popmarket.ts
@@ -7,6 +7,7 @@ import Button from './button';
 import MarketItem from './marketitem';
 import theSea from './theSea';
 import SingletonClass from './singleton';
+import EconomyItem from './economyitem';
 
 export default class popMarket extends PopUp
 {
@@ -14,8 +15,11 @@ export default class popMarket extends PopUp
     private txtMouseOver:PIXI.Text;
     private txtPrice:PIXI.Text;
     private coin:PIXI.Sprite;
-    private marketItems:Array<MarketItem>;
+    private marketItems:Array<MarketItem> = [];
     private marketData:any;
+    private pageIndex:number=0; // index of the market item for this page of results
+    private pageUp:Button;
+    private pageDown:Button;
 
     constructor()
     {
@@ -27,17 +31,26 @@ export default class popMarket extends PopUp
         super.init(); // background and X button with handler
         var data = this.getMarketData();
         var i,m,rate,up;
-        for (i=0; i<8; i++)
+        for (i=0; i<EconomyItem.maxItems; i++)
         {
             rate = data[i].rate;
             up = data[i].up;
             m = new MarketItem(i,rate,up);
-            m.x = 113;
-            m.y = 94 + (i * (m.height + 5));
             this.addChild(m);
+            if (i<8) // position and add the first page
+            {
+                m.x = 113;
+                m.y = 94 + (i * (m.height + 5));
+                m.visible = true;
+            } else {
+                m.visible = false;
+            }
+
             //console.log("item: " + i + "rate: " + rate + " up: " + up);
             m.interactive = true;
             m.on('mouseover',this.itemOver);
+            
+            this.marketItems.push(m);
         }
 
         var styleb = new PIXI.TextStyle({
@@ -77,6 +90,55 @@ export default class popMarket extends PopUp
         this.txtPrice.x = this.coin.x + this.coin.width + 20;
         this.txtPrice.y = 480;
         this.addChild(this.txtPrice);
+
+        this.pageDown = new Button(PIXI.Texture.fromFrame("uiArrow.png"));
+        this.pageDown.x = 696;
+        this.pageDown.y = 463;
+        this.addChild(this.pageDown);
+        this.pageDown.on('click',this.onPageDown);
+
+        this.pageUp = new Button(PIXI.Texture.fromFrame("uiArrow.png"));
+        this.pageUp.rotation = Math.PI; // 180 degrees
+        this.pageUp.x = 696;
+        this.pageUp.y = 103;
+        this.addChild(this.pageUp);
+        this.pageUp.on('click', this.onPageUp);
+    }
+
+    onPageDown = () =>
+    {
+        this.pageIndex += 8;
+        if (this.pageIndex > 55)
+            this.pageIndex -= 8;
+        this.displayPage();
+    }
+
+    onPageUp = () =>
+    {
+        this.pageIndex -= 8;
+        if (this.pageIndex < 0)
+            this.pageIndex = 0;
+        this.displayPage();
+    }
+
+    private displayPage()
+    {
+        // display 8 market items from pageIndex member, hide all others
+        var i,k,m;
+        for (i=0;i<this.marketItems.length;i++)
+        {
+            m = this.marketItems[i];
+            if (i >= this.pageIndex && i <= this.pageIndex + 7)
+            {  
+                k = i - this.pageIndex;
+                console.log("i:" + i + " k:"+ k + " index: " + this.pageIndex);
+                m.x = 113;
+                m.y = 94 + (k * (m.height + 5));
+                m.visible = true;
+            } else {
+                m.visible = false;
+            }
+        }
     }
 
     private getMarketData()
